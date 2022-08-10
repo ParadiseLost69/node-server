82c4cc3e91c45dcf29f4f6677da44762f72f3d21
Teddy <tedloukas@outlook.com>
2022-08-10 23:43:22 +0900

    Create Node.js server with routing

---
+++ index.js
@@ -0,0 +1,32 @@
+const http = require("http");
+const fs = require("fs");
+const PORT = 3000;
+const server = http.createServer((req, res) => {
+  res.setHeader("Content-Type", "text/html");
+
+  let path = "./views/";
+
+  switch (req.url) {
+    case "/":
+      path += "index.html";
+      res.statusCode = 200;
+      break;
+    default:
+      path += "404.html";
+      res.statusCode = 404;
+      break;
+  }
+
+  fs.readFile(path, (err, data) => {
+    if (err) {
+      console.log(err);
+      res.end();
+    } else {
+      res.end(data);
+    }
+  });
+});
+
+server.listen(PORT, () => {
+  console.log("listening...");
+});
