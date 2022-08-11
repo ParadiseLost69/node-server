090987376415b3a82423e55de5385f6681e7f2ab
Teddy <tedloukas@outlook.com>
2022-08-11 19:44:26 +0900

    Implement EJS

--- app.js
+++ app.js
@@ -1,7 +1,6 @@
 const express = require("express");
 
 //express app
-
 const app = express();
 
 //register view engine
@@ -11,19 +10,30 @@ app.listen(3000);
 
 app.get("/", (req, res) => {
   //   res.send("<p> home page </p>");
-  res.sendFile("./views/index.html", { root: __dirname });
+  const blogs = [
+    { title: "Yoshi finds eggs", snippet: " lorem ipsum dolor sit amet" },
+    {
+      title: "Mario finds a star",
+      snippet: " Mario did it. The mad lad. I can't believe it.",
+    },
+    {
+      title: "Birdo is the best",
+      snippet: "All hail our supreme overlord and sexual icon Birdo.",
+    },
+  ];
+  res.render("index", { title: "Ninja", blogs });
 });
 
 app.get("/about", (req, res) => {
-  res.sendFile("./views/about.html", { root: __dirname });
+  res.render("about", { title: "ABOUT" });
 });
 
-app.get("/about-us", (req, res) => {
-  res.redirect("/about");
+app.get("/blogs/create", (req, res) => {
+  res.render("create", { title: "Create a new Blog" });
 });
 
 //404 for Middleware
 
 app.use((req, res) => {
-  res.status(404).sendFile("./views/404.html", { root: __dirname });
+  res.status(404).render("404", { title: "Error" });
 });
